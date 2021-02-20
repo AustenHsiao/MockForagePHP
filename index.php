35bd8cf9af53cc8dd6cdc1993d09c86d3031ee0f
AustenHsiao <Austenhsiao@gmail.com>
2021-02-20 15:51:58 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="authors" content="Austen Hsiao" />
+    <title>Secret Spot</title>
+    <link rel="stylesheet" href="/style/forage.css" />
+  </head>
+  <body>
+    
+    <nav class="container navBar">
+        <h3>Secret Spot</h3>
+        <a href="/" name="nav1" id="nav1" >Main</a>
+        <a href="/" name="nav2" id ="nav2">Add Spot</a>
+        <a href="/" name="nav3" id="nav3">About</a>
+    </nav>
+    <?php   //Look up the database     
+        function lookup_locations($username_input){
+            $db = parse_url(getenv("DATABASE_URL"));
+            $db["path"] = ltrim($db["path"], "/");
+
+        }
+    ?>
+    <div class="container whole">
+      <div class="container coord">
+        <h3 class='coordTitle' id='searchBar'>Search by User Name</h3>
+        <input id='userNameInput' type='text' name='username_input' placeholder='Enter name'> <!-- place php here-->
+        <div class='searchSplit'> 
+            <h3 class="coordTitle">Foraging Locations</h3>
+            <div class="searchResult"></div>
+        </div>
+      </div>
+
+      <div class="container map">
+        <div id="mapBox">
+            <img src="https://i.imgur.com/syTPyyp.png" alt='Google maps mock image'/>
+        </div>
+      </div>
+    </div>
+    <div class="container weather" id="weatherBox"></div>
+  </body>
+</html>
