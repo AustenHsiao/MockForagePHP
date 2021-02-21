87cd3653f70b2a429ae5ce3fbc9881bc1242bc93
AustenHsiao <Austenhsiao@gmail.com>
2021-02-20 18:55:24 -0800

    Testing button

--- index.php
+++ index.php
@@ -14,49 +14,50 @@
         <a href="/" name="nav2" id ="nav2">Add Spot</a>
         <a href="/" name="nav3" id="nav3">About</a>
     </nav>
-    <?php   
-        //Look up the database     
-        function lookup_locations(){
-            $db = parse_url(getenv("DATABASE_URL"));
-            $db["path"] = ltrim($db["path"], "/");
-            $connection_string = "host=" . $db["host"] . " dbname=" . $db["path"] . " user=" . $db["user"] . " password=" . $db["pass"];
-            $connect = pg_connect($connection_string);
-
-            if($connect){   
-                /*$fullname = preg_split(" ", $_POST["username_input"]);
-                $escape_first = mysql_real_escape_string(trim($fullname[0]));
-                $escape_last = mysql_real_escape_string(trim($fullname[1]));*/
-                $statement = "SELECT * FROM locations L JOIN users U ON L.user=U.id"/* WHERE U.name_first=$escape_first AND U.name_last=$escape_last"*/;
-                $result = pg_query($connect, $statement);
-                while($row = pg_fetch_assoc($result)){
-                    echo $row['name_first'] . "\n";
-                    echo $row['name_last'] . "\n";
-                    echo $row['title'] . "\n";
-                    echo $row['comment'] . "\n";
-                    echo $row['lat'] . "\n";
-                    echo $row['lng'] . "\n";
-                    echo "\n";
-                }
-
-                pg_close($connect);
-            }else{
-                echo "Not connected";
-            }
-        }
-    ?>
     <div class="container whole">
       <div class="container coord">
         <h3 class="coordTitle" id="searchBar">Search by User Name</h3>
-        <input id="userNameInput" type="text" name="username_input" placeholder="Enter name"> <!-- place php here-->
-        <!--<input type='button' value="Search" name="search_btn">-->
-        <p>
-            <?php
-                lookup_locations(/*$_POST['username_input']*/);
-            ?>
-        </p>
+        <input id="userNameInput" type="text" name="username_input" placeholder="Enter name">
+        <input type='button' value="Search" name="search_btn">
+            
         <div class='searchSplit'> 
             <h3 class="coordTitle">Foraging Locations</h3>
-            <div class="searchResult"></div>
+            <div class="searchResult">
+                <?php
+                    //Look up the database     
+                    function lookup_locations(){
+                        $db = parse_url(getenv("DATABASE_URL"));
+                        $db["path"] = ltrim($db["path"], "/");
+                        $connection_string = "host=" . $db["host"] . " dbname=" . $db["path"] . " user=" . $db["user"] . " password=" . $db["pass"];
+                        $connect = pg_connect($connection_string);
+
+                        if($connect){   
+                            $fullname = preg_split(" ", $_POST["username_input"]);
+                            $escape_first = mysql_real_escape_string(trim($fullname[0]));
+                            $escape_last = mysql_real_escape_string(trim($fullname[1]));*/
+                            $statement = "SELECT name_first, name_list, title, comment, lat, lng FROM locations L JOIN users U ON L.user=U.id WHERE U.name_first=$escape_first AND U.name_last=$escape_last";
+                            $result = pg_query($connect, $statement);
+                            while($row = pg_fetch_assoc($result)){
+                                echo $row['name_first'] . "\n";
+                                echo $row['name_last'] . "\n";
+                                echo $row['title'] . "\n";
+                                echo $row['comment'] . "\n";
+                                echo $row['lat'] . "\n";
+                                echo $row['lng'] . "\n";
+                                echo "\n\n";
+                            }
+
+                            pg_close($connect);
+                        }else{
+                            echo "Not connected";
+                        }
+                    }
+
+                    if(array_key_exists('search_btn', $_POST)){
+                        lookup_locations();
+                    }
+                ?>
+            </div>
         </div>
       </div>
 
